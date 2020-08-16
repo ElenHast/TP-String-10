edcf95400939849919ec3dbc4656eefb8d8a1dcd
MRM <55654081+ElenHast@users.noreply.github.com>
2020-08-16 20:12:39 -0300

    Update Ejercicio 6.js

--- Ejercicio 6.js
+++ Ejercicio 6.js
@@ -8,3 +8,21 @@ var palabrasConP = 0;
 while (string06 > i) {
   if(string06[i] == "p"){palabrasConP++;}else{i++;}
 }
+
+// Una prueba de como podria ser.
+let unTexto = "Vamos a ver si ahora el programa funcion bien por las dudasp";
+let i = 0;
+let palabras = 0;
+let caracteres = 0;
+while(i < unTexto.length){
+    while(unTexto[i] == " "){
+        i++;
+    }
+    while(unTexto[i-1] == " "){
+        if(unTexto[i] == "p" || unTexto[i] == "P"){
+           palabras++;}
+        i++;
+    }
+    i++;
+}
+console.log(palabras);
