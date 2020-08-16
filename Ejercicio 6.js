a065cf24e392edcf0dedd87b3241f0b025c5cce5
MRM <55654081+ElenHast@users.noreply.github.com>
2020-08-16 20:12:02 -0300

    Create Ejercicio 6.js

---
+++ Ejercicio 6.js
@@ -0,0 +1,10 @@
+/*6. Hacer un programa que permita contar la cantidad de palabras que
+comienzan con “P” de una cadena de caracteres.*/
+
+//Todavia sin terminar
+var string06 = "Hola Pedro, como estas? pelando papas?.";
+var i = 0;
+var palabrasConP = 0;
+while (string06 > i) {
+  if(string06[i] == "p"){palabrasConP++;}else{i++;}
+}
