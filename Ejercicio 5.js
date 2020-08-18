7bc880d3a2d84269ade6e77da9163d495fb466cb
MRM <55654081+ElenHast@users.noreply.github.com>
2020-08-18 00:08:18 -0300

    Update Ejercicio 5.js

--- Ejercicio 5.js
+++ Ejercicio 5.js
@@ -36,3 +36,22 @@ function texto(texto) {
 }
 
 console.log(texto("Hola como andas todo bien chamigo"));
+
+// hecho por gustavo, funca re bien
+var fort = "San martin era un tipo re groso.";
+var i = 0
+var palabras = 0
+var palabrasLargas = 0
+while(fort[i] != "."){
+    while(fort[i] == " "){ i++};
+    palabras = "";
+    while(fort[i] != " " && fort[i] != "."){
+        palabras = palabras + fort[i];
+        i++;
+    }
+    if(palabras.length > 3){
+        palabrasLargas++;
+    }
+}
+
+console.log("Las palabras que contienen mas de 3 caracteres son en total: " + palabrasLargas);
