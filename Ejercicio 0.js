58f9e2a84cee691b01e1d9c55b9e11574c77f016
MRM <55654081+ElenHast@users.noreply.github.com>
2020-08-18 01:04:30 -0300

    Create Ejercicio 0.js

---
+++ Ejercicio 0.js
@@ -0,0 +1,15 @@
+let cadena = "Prueba de texto.";
+let i = 0;
+while (i < cadena.length){
+  //recorrido general de la cadena
+  while (cadena[i] == " "){
+    i++;
+  }
+  // Al inicio de una palabras
+  while((cadena[i] != " ")&&(i < cadena.length)){
+  // recorrido de los caracteres
+    i++;
+  }
+  // Al final de la palabras
+}
+// Final del procesamiento
