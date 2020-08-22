0be89b35a919c845a29ec746baffff12deb20301
MRM <55654081+ElenHast@users.noreply.github.com>
2020-08-21 23:57:17 -0300

    Update Ejercicio 8.js

--- Ejercicio 8.js
+++ Ejercicio 8.js
@@ -1,3 +1,46 @@
 /*8. Hacer un script que dado una cadena de caracteres genera una nueva
 cadena con todas las palabras que comienzan con el caracter “A” y finalizan
 con el caracter “R”.*/
+
+function cadenaAR(texto) {
+  let i = 0;
+  let palabra;
+  let cantidad = 0;
+  let lista = "";
+  while (texto[i]!="."){
+   while(texto[i]==" "){
+     i++;
+   }
+   palabra = "";
+   while(texto[i]!=" "&&texto[i]!="."){
+     palabra = palabra+texto[i];
+     i++;
+   }
+   
+   if(palabra.endsWith("ar")){
+     lista = lista+" "+palabra;
+   }
+  }
+}
+
+let texto = "Afilar y adornar el cuchillo.";
+let i = 0;
+let palabra;
+let cantidad = 0;
+let lista = "";
+while (texto[i]!="."){
+ while(texto[i]==" "){
+   i++;
+ }
+ palabra = "";
+ while(texto[i]!=" "&&texto[i]!="."){
+   palabra = palabra+texto[i];
+   i++;
+ }
+ 
+ if(palabra.endsWith("ar")){
+   lista = lista+" "+palabra;
+    
+   console.log(lista);
+ }
+}
